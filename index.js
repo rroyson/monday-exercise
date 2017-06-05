94545fce35e834aba7141731dea51d50644ed5e8
rroyson <frontiersons@gmail.com>
2017-06-05 10:59:08 -0400

    2nd log

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 
 const R = require('ramda')
 
+//remove last item "rollno"
+
+var student = {
+ name : "David Rayy",
+ sclass : "VI",
+ rollno : 12 }
+
+delete student["rollno"]
+
+console.log(student)
+
+
+
 const teams = [
   { id: 1, name: "Braves", league: "NL" },
   { id: 2, name: "Yankees", league: "AL" },
@@ -11,6 +24,8 @@ const teams = [
   { id: 6, name: "Cubs", league: "NL" }
 ];
 
+//select first three
+
 function firstThree(teams) {
   return teams.filter(idSelector);
 }
@@ -19,20 +34,22 @@ function idSelector(team) {
   return team.id <= 3;
 }
 
+console.log(firstThree(teams));
+
+//select league
+
 function leagueName (teams) {
   return teams.filter(leagueSelector)
 }
  function leagueSelector(team) {
    return team.league === "NL" || "AL"
  }
+console.log(leagueName(teams))
+
+//flip keys
 
 const flipKeys = function(team) {
   return R.invertObj(team)
 }
 
-
-
-
-console.log(firstThree(teams));
-console.log(leagueName(teams))
 console.log(teams.map(flipKeys))
--- readme.md
+++ readme.md
@@ -1,1 +1,25 @@
-markdown
+# 2017-06-05-rroyson
+
+JS exercise with node and github
+
+## Remove rollno
+Removes the Rollno value
+
+call student
+
+##firstThree
+grabs first three items from the array teams
+
+## leagueName
+grabs teams with specific league name
+
+## filp keys
+switches keys and values in the array teams
+
+
+
+to run program, run the following in the terminal:
+
+```
+$node index.js
+```
